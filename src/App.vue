832a2e418785df50667053ac3b8fe2ce3799dc50
fishmandev <fishman.code@gmail.com>
2017-07-11 09:00:08 +0300

    Simple fetching data implementation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <router-link to="/">Home</router-link> | <router-link to="/list">List</router-link>
     <router-view></router-view>
   </div>
 </template>
